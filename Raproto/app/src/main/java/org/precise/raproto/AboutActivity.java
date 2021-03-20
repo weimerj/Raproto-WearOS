9929bf83073a28f416e0b40cbf1a4b7454d1c96c
Amanda Watson <aawatson@seas.upenn.edu>
2021-03-19 21:58:45 -0400

    Added Version

--- Raproto/app/src/main/java/org/precise/raproto/AboutActivity.java
+++ Raproto/app/src/main/java/org/precise/raproto/AboutActivity.java
@@ -1,44 +1,49 @@
 package org.precise.raproto;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
 import androidx.wear.ambient.AmbientModeSupport;
 
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends FragmentActivity implements
-        AmbientModeSupport.AmbientCallbackProvider {
+public class AboutActivity extends FragmentActivity
+        implements AmbientModeSupport.AmbientCallbackProvider {
 
     private List<ListsItem> mItems;
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
 
         AmbientModeSupport.attach(this);
 
         // Create a list of items for adapter to display.
         mItems = new ArrayList<>();
-        mItems.add(new ListsItem(R.string.app_name));
-        mItems.add(new ListsItem(R.string.device_id));
-        mItems.add(new ListsItem(R.string.on_off));
-        mItems.add(new ListsItem(R.string.sync));
-        mItems.add(new ListsItem(R.string.settings));
-        mItems.add(new ListsItem(R.string.about, AboutActivity.class));
+        mItems.add(new ListsItem(R.string.version));
+        mItems.add(new ListsItem(R.string.info));
+        mItems.add(new ListsItem(R.string.disclamer));
+        mItems.add(new ListsItem(R.string.help));
 
         // Initialize an adapter and set it to ListView listView.
         ListViewAdapter adapter = new ListViewAdapter(this, mItems);
-        final ListView listView = findViewById(R.id.list_view_lists);
+        final ListView listView = findViewById(R.id.about_lists);
         listView.setAdapter(adapter);
+
+        // Set header of listView to be the title from title_layout.
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View titleLayout = inflater.inflate(R.layout.title_layout, null);
+        TextView titleView = titleLayout.findViewById(R.id.title_text);
+        titleView.setText(R.string.about);
+        titleView.setOnClickListener(null); // make title non-clickable.
+
+        listView.addHeaderView(titleView);
 
         // Goes to a new screen when you click on one of the list items.
         // Dependent upon position of click.
@@ -51,12 +56,11 @@ public class MainActivity extends FragmentActivity implements
                                 .launchActivity(getApplicationContext());
                     }
                 });
-
     }
 
     @Override
     public AmbientModeSupport.AmbientCallback getAmbientCallback() {
-        return null;
+        return new MyAmbientCallback();
     }
 
     private class MyAmbientCallback extends AmbientModeSupport.AmbientCallback {}
