1ccb8ff6f816d0f7012fc228bb606dfa2c4cf9b9
Amanda Watson <aawatson@seas.upenn.edu>
2021-03-19 21:07:56 -0400

    List of Activities

--- Raproto/app/src/main/java/org/precise/raproto/MainActivity.java
+++ Raproto/app/src/main/java/org/precise/raproto/MainActivity.java
@@ -1,14 +1,20 @@
 package org.precise.raproto;
 
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
 import androidx.wear.ambient.AmbientModeSupport;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends FragmentActivity implements
         AmbientModeSupport.AmbientCallbackProvider {
+
+    private List<ListsItem> mItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,22 @@ public class MainActivity extends FragmentActivity implements
         setContentView(R.layout.activity_main);
 
         AmbientModeSupport.attach(this);
+
+        // Create a list of items for adapter to display.
+        mItems = new ArrayList<>();
+        mItems.add(new ListsItem(R.string.app_name));
+        mItems.add(new ListsItem(R.string.device_id));
+        mItems.add(new ListsItem(R.string.on_off));
+        mItems.add(new ListsItem(R.string.sync));
+        mItems.add(new ListsItem(R.string.settings));
+        mItems.add(new ListsItem(R.string.about));
+
+
+
+        // Initialize an adapter and set it to ListView listView.
+        ListViewAdapter adapter = new ListViewAdapter(this, mItems);
+        final ListView listView = findViewById(R.id.list_view_lists);
+        listView.setAdapter(adapter);
 
     }
 
