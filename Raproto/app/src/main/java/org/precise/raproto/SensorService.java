d3a3b709c4b2bef7997b8d5e3ceaf322ed3187b6
Amanda Watson <aawatson@seas.upenn.edu>
2021-04-06 07:45:16 -0400

    All Sensors Added

--- Raproto/app/src/main/java/org/precise/raproto/SensorService.java
+++ Raproto/app/src/main/java/org/precise/raproto/SensorService.java
@@ -7,31 +7,23 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.widget.Toast;
 
 public class SensorService extends Service implements SensorEventListener {
 
-    public SensorService() {
-    }
     private SensorManager mSensorManager;
     private Sensor mSensor;
-    private long lastUpdate = 0;
-    private float last_x, last_y, last_z;
-    private static final int SHAKE_THRESHOLD = 600;
 
+    public SensorService() {
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        String android_id = Settings.Secure.getString(SensorService.this.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
 
-        // Get sensor manager on starting the service.
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        // Registering...
-        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        // Get default sensor type
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     @Override
@@ -40,55 +32,77 @@ public class SensorService extends Service implements SensorEventListener {
         // Get sensor manager on starting the service.
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        // Registering...
-        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        // Get default sensor type
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        // Registering Sensors
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE), SensorManager.SENSOR_DELAY_NORMAL);
 
         return START_STICKY;
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(this);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-
-        //Toast.makeText(getApplicationContext(),"Collecting Accel Data",Toast.LENGTH_SHORT).show();
-
-        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        Sensor mySensor = sensorEvent.sensor;
-
-        mSensorManager.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-
-            float[] values = sensorEvent.values;
-            float x = values[0];
-            float y = values[1];
-            float z = values[2];
-
-            long curTime = System.currentTimeMillis();
-            if ((curTime - lastUpdate) > 100) {
-                long diffTime = (curTime - lastUpdate);
-                lastUpdate = curTime;
-                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
-                last_x = x;
-                last_y = y;
-                last_z = z;
-            }
-            // Stop the sensor and service
-            mSensorManager.unregisterListener(this);
-            stopSelf();
-        }
+        return null;
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent  ) {
+
+        //Toast.makeText(getApplicationContext(),"Collecting Accel Data",Toast.LENGTH_SHORT).show();
+
+        //"{\"ts\": \"%llu\",\"values\"={\"%s_SYS\":{\"Bat\":%d}}}", tsLong, android_id, battery_percent);
+        switch(sensorEvent.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                float accel_x = sensorEvent.values[0];
+                float accel_y = sensorEvent.values[1];
+                float accel_z = sensorEvent.values[2];
+                long tsLong = System.currentTimeMillis()/1000;
+                //TODO: JSON Formatting
+                break;
+
+            case Sensor.TYPE_GYROSCOPE:
+                float gyro_x = sensorEvent.values[0];
+                float gyro_y = sensorEvent.values[1];
+                float gyro_z = sensorEvent.values[2];
+                tsLong = System.currentTimeMillis()/1000;
+                //TODO: JSON Formatting
+                break;
+
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                float mag_x = sensorEvent.values[0];
+                float mag_y = sensorEvent.values[1];
+                float mag_z = sensorEvent.values[2];
+                tsLong = System.currentTimeMillis()/1000;
+                //TODO: JSON Formatting
+                break;
+
+            case Sensor.TYPE_GRAVITY:
+                float grav_x = sensorEvent.values[0];
+                float grav_y = sensorEvent.values[1];
+                float grav_z = sensorEvent.values[2];
+                tsLong = System.currentTimeMillis()/1000;
+                //TODO: JSON Formatting
+                break;
+
+            case Sensor.TYPE_HEART_RATE:
+                float hrm = sensorEvent.values[0];
+                tsLong = System.currentTimeMillis()/1000;
+                //TODO: JSON Formatting
+                break;
+        }
+    }
+
+
 }
