d009deef74159b95066fef5b3803279c9871b634
DESKTOP-6V8LD4B\tarek <hamidtarek3@gmail.com>
2022-05-11 20:56:31 -0400

    Updating attribute subscribe to Screen Config class

--- Raproto/app/src/main/java/org/precise/raproto/MQTTService.java
+++ Raproto/app/src/main/java/org/precise/raproto/MQTTService.java
@@ -30,7 +30,6 @@ public class MQTTService extends Service {
     String topic = "v1/devices/me/telemetry";
     private String username = "fbdb89251fdc95aa"; //Access token for device
     private String password = ""; //leave empty
-    int qos = 1;
 
     private DatabaseHandler db;
     MqttAndroidClient client;
@@ -68,8 +67,6 @@ public class MQTTService extends Service {
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // We are connected
                     Log.d(TAG, "Connected Successfully.");
-                    subscribeToAttributesTopic();
-                    getSharedAttributes();
                 }
 
                 @Override
@@ -116,73 +113,6 @@ public class MQTTService extends Service {
             mHandler.postDelayed(this, 20000);
         }
     };
-
-    /**
-     * Subscribes to attribute updates from Thingsboard and adds to shared preferences.
-     */
-    private void subscribeToAttributesTopic() {
-        try {
-            Log.d(TAG, "Subscribing..");
-            client.subscribe("v1/devices/me/attributes/response/+", qos);
-            client.setCallback(new MqttCallback() {
-                @Override
-                public void connectionLost(Throwable cause) {
-                    Log.d(TAG, "Connection Lost.");
-                }
-
-                @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    Log.d(TAG, "Message arrived.");
-                    Log.d(TAG,"message>>" + new String(message.getPayload()));
-                    Log.d(TAG,"topic>>" + topic);
-
-                    // Add values to shared preferences obj
-                    JSONObject response = new JSONObject(message.toString());
-                    JSONObject sharedAttributes = response.getJSONObject("shared");
-                    JSONArray sharedAttributesKeys = sharedAttributes.names();
-
-                    for(int i = 0; i < sharedAttributes.length(); i++){
-                        String key = sharedAttributesKeys.getString(i);
-                        String value = sharedAttributes.getString(key);
-                        SharedPreferences.Editor editor = sharedPref.edit();
-                        editor.putString(key, value);
-                        editor.apply();
-                    }
-
-                    // Confirm values added to sharedPref
-                    /*
-                    Map<String, ?> allEntries = sharedPref.getAll();
-                    for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
-                        Log.d("map values", entry.getKey() + ": " + entry.getValue().toString());
-                    }
-                     */
-                }
-
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                    Log.d(TAG, "Subscription Successful.");
-                }
-            });
-        } catch (MqttException e){
-            Log.d(TAG, "Exception occurred " + e);
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Requests attribute values from Thingsboard using publish topic.
-     */
-    private void getSharedAttributes() {
-        try {
-            String attributesMessage = "{'GRAVITY': '-1'}";
-            MqttMessage message = new MqttMessage(attributesMessage.getBytes("UTF-8"));
-            client.publish("v1/devices/me/attributes/request/1", message);
-            Log.d(TAG, "Requested shared attributes.");
-        } catch (UnsupportedEncodingException | MqttException e) {
-            Log.d(TAG, "Exception occurred " + e);
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public void onDestroy() {
