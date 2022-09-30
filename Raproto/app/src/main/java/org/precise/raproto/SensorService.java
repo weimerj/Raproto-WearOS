85245492f0ae29b9fc4c8ed2b150c6d2eaaacf33
Kimberly Helm <kimhelm@seas.upenn.edu>
2022-09-30 07:45:39 -0700

    TX_RATE and QOS shared preferences implemented

--- Raproto/app/src/main/java/org/precise/raproto/SensorService.java
+++ Raproto/app/src/main/java/org/precise/raproto/SensorService.java
@@ -1,28 +1,32 @@
+
+
 package org.precise.raproto;
 
-import static android.hardware.Sensor.TYPE_HEART_RATE;
-
-import android.Manifest;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.BatteryManager;
 import android.os.IBinder;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.content.SharedPreferences;
 
+import androidx.core.app.NotificationCompat;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 
 public class SensorService extends Service implements SensorEventListener {
     private final static int BUFFER_THRESHOLD = 1024 * 10;
@@ -30,7 +34,6 @@ public class SensorService extends Service implements SensorEventListener {
     private DatabaseHandler db;
     private String TAG = SENSOR_SERVICE;
     private Intent batteryStatus;
-    private MenuMain mMain = new MenuMain();
 
     StringBuffer buffer = new StringBuffer(BUFFER_THRESHOLD);
     JSONArray jsonArray = new JSONArray();
@@ -38,11 +41,11 @@ public class SensorService extends Service implements SensorEventListener {
     public SensorService() {
     }
 
+
     @Override
     public void onCreate() {
-
-        PackageManager packman = getPackageManager();
         super.onCreate();
+
 
         //Create Database handler
         db = new DatabaseHandler(this);
@@ -52,17 +55,10 @@ public class SensorService extends Service implements SensorEventListener {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        // Get sensor manager on starting the service.
+        //Get sensor manager upon starting the service.
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        // Battery filter register
-        final IntentFilter batterIntentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
 
-        // Registering Sensors
-//        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-//        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
-//        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_NORMAL);
-//        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE), SensorManager.SENSOR_DELAY_NORMAL);
-
+        //Import shared preferences
         SharedPreferences sharedPref = getSharedPreferences("Raproto", Context.MODE_PRIVATE);
 
         //Register ACC sensor
@@ -73,7 +69,7 @@ public class SensorService extends Service implements SensorEventListener {
         //Register GYRO sensor
         if (sharedPref.getInt("GYRO", -1) != -1) {
             mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), sharedPref.getInt("GYRO", -1) * 1000, sharedPref.getInt("GYRO", -1) * 1000);
-            }
+        }
 
         //Register GRAVITY sensor
         if (sharedPref.getInt("GRAVITY", -1) != -1) {
@@ -86,6 +82,7 @@ public class SensorService extends Service implements SensorEventListener {
         }
 
         //Register battery sensor
+        final IntentFilter batterIntentFilter = new IntentFilter(intent.ACTION_BATTERY_CHANGED);
         batteryStatus = this.registerReceiver(null, batterIntentFilter);
 
         return START_STICKY;
@@ -113,7 +110,7 @@ public class SensorService extends Service implements SensorEventListener {
                 Settings.Secure.ANDROID_ID);
 
 
-        if(jsonArray.toString().getBytes().length < BUFFER_THRESHOLD){
+        if (jsonArray.toString().getBytes().length < BUFFER_THRESHOLD) {
             //put sensor value and timestamp to JSON string
             switch (sensorEvent.sensor.getType()) {
                 case Sensor.TYPE_ACCELEROMETER:
@@ -128,15 +125,18 @@ public class SensorService extends Service implements SensorEventListener {
                     jsonArray.put(getGravityJson(sensorEvent, android_id));
                     break;
 
-                case TYPE_HEART_RATE:
+                case Sensor.TYPE_HEART_RATE:
                     //TODO: Look into Green light vs red light
                     jsonArray.put(getHRMJson(sensorEvent, android_id));
                     break;
             }
-        }
-        else{
+        } else {
             //include battery data to every buffer
-            jsonArray.put(getBatteryJson(android_id));
+            SharedPreferences sharedPref = getSharedPreferences("Raproto", Context.MODE_PRIVATE);
+
+            if (sharedPref.getInt("BATTERY", -1) != -1) {
+                jsonArray.put(getBatteryJson(android_id));
+            }
 
             Log.d(TAG, jsonArray.toString());
             try {
@@ -148,9 +148,12 @@ public class SensorService extends Service implements SensorEventListener {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+
         }
     }
+
     public JSONObject getBatteryJson(String android_id) {
+
         JSONObject LevelThrJson = new JSONObject();
         JSONObject LevelTwoJson = new JSONObject();
         JSONObject LevelOneJson = new JSONObject();
@@ -159,6 +162,7 @@ public class SensorService extends Service implements SensorEventListener {
         int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
         int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+
         float batteryPot = level * 100 / (float) scale;
         boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING;
         long tsLong = System.currentTimeMillis();
@@ -170,7 +174,7 @@ public class SensorService extends Service implements SensorEventListener {
             LevelTwoJson.put(android_id + "_BAT", LevelThrJson);
             LevelOneJson.put("ts", tsLong);
             LevelOneJson.put("values", LevelTwoJson);
-        }catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         return LevelOneJson;
@@ -180,6 +184,7 @@ public class SensorService extends Service implements SensorEventListener {
         JSONObject LevelThrJson = new JSONObject();
         JSONObject LevelTwoJson = new JSONObject();
         JSONObject LevelOneJson = new JSONObject();
+        DecimalFormat precision = new DecimalFormat("0.000");
 
         float accel_x = sensorEvent.values[0];
         float accel_y = sensorEvent.values[1];
@@ -187,9 +192,9 @@ public class SensorService extends Service implements SensorEventListener {
         long tsLong = System.currentTimeMillis();
 
         try {
-            LevelThrJson.put("x", accel_x);
-            LevelThrJson.put("y", accel_y);
-            LevelThrJson.put("z", accel_z);
+            LevelThrJson.put("x", precision.format(accel_x));
+            LevelThrJson.put("y", precision.format(accel_y));
+            LevelThrJson.put("z", precision.format(accel_z));
             LevelTwoJson.put(android_id + "_ACC", LevelThrJson);
             LevelOneJson.put("ts", tsLong);
             LevelOneJson.put("values", LevelTwoJson);
@@ -203,6 +208,7 @@ public class SensorService extends Service implements SensorEventListener {
         JSONObject LevelThrJson = new JSONObject();
         JSONObject LevelTwoJson = new JSONObject();
         JSONObject LevelOneJson = new JSONObject();
+        DecimalFormat precision = new DecimalFormat("0.000");
 
         float gyro_x = sensorEvent.values[0];
         float gyro_y = sensorEvent.values[1];
@@ -210,9 +216,9 @@ public class SensorService extends Service implements SensorEventListener {
         long tsLong = System.currentTimeMillis();
 
         try {
-            LevelThrJson.put("x", gyro_x);
-            LevelThrJson.put("y", gyro_y);
-            LevelThrJson.put("z", gyro_z);
+            LevelThrJson.put("x", precision.format(gyro_x));
+            LevelThrJson.put("y", precision.format(gyro_y));
+            LevelThrJson.put("z", precision.format(gyro_z));
             LevelTwoJson.put(android_id + "_GYRO", LevelThrJson);
             LevelOneJson.put("ts", tsLong);
             LevelOneJson.put("values", LevelTwoJson);
@@ -227,6 +233,7 @@ public class SensorService extends Service implements SensorEventListener {
         JSONObject LevelThrJson = new JSONObject();
         JSONObject LevelTwoJson = new JSONObject();
         JSONObject LevelOneJson = new JSONObject();
+        DecimalFormat precision = new DecimalFormat("0.000");
 
         float grav_x = sensorEvent.values[0];
         float grav_y = sensorEvent.values[1];
@@ -235,9 +242,9 @@ public class SensorService extends Service implements SensorEventListener {
         long tsLong = System.currentTimeMillis();
 
         try {
-            LevelThrJson.put("x", grav_x);
-            LevelThrJson.put("y", grav_y);
-            LevelThrJson.put("z", grav_z);
+            LevelThrJson.put("x", precision.format(grav_x));
+            LevelThrJson.put("y", precision.format(grav_y));
+            LevelThrJson.put("z", precision.format(grav_z));
             LevelTwoJson.put(android_id + "_GRAVITY", LevelThrJson);
             LevelOneJson.put("ts", tsLong);
             LevelOneJson.put("values", LevelTwoJson);
@@ -252,6 +259,7 @@ public class SensorService extends Service implements SensorEventListener {
         JSONObject LevelThrJson = new JSONObject();
         JSONObject LevelTwoJson = new JSONObject();
         JSONObject LevelOneJson = new JSONObject();
+        DecimalFormat precision = new DecimalFormat("0.000");
 
         float hrm = sensorEvent.values[0];
         long tsLong = System.currentTimeMillis();
